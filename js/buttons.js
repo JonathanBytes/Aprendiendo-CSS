b96755efa24e2c5443b955df6a945767fa271c37
JonathanWin <jonathancardonaramirez@gmail.com>
2023-04-25 19:37:59 -0500

    Mobile navbar animation

--- js/buttons.js
+++ js/buttons.js
@@ -1,18 +1,14 @@
 const button = document.querySelector(".button-three");
-const btnContact = document.querySelector(".btn");
-const nav = document.querySelector(".nav");
 const mobile = document.querySelector(".mobile-nav")
 
 button.addEventListener("click", () => {
-  const currentState = button.getAttribute("data-state");
+  const currentState = mobile.getAttribute("data-visible");
 
-  if (!currentState || currentState === "closed") {
-    button.setAttribute("data-state", "opened");
+  if (!currentState || currentState === "false") {
     button.setAttribute("aria-expanded", "true");
-    mobile.style.display = "flex";
+    mobile.setAttribute("data-visible", "true");
   } else {
-    button.setAttribute("data-state", "closed");
     button.setAttribute("aria-expanded", "false");
-    mobile.style.display = "none";
+    mobile.setAttribute("data-visible", "false");
   }
 });
